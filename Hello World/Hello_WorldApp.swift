a3b365244be184cc4ed480812e45a74c4131a6e5
Sora Torres <kaithemaido@gmail.com>
2022-08-31 09:33:00 -0500

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Sora Torres on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
